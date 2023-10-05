38c6a612b1c25e109999922a7248f6d6bdbf8a19
Matt <mattleroy.dev@pm.me>
2023-10-05 13:20:19 -0400

    Added new javascript file, added placeholder images for testing, and additional styling

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+function showPopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = 'block';
+}
+
+function hidePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = 'none';
+}
